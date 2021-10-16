0344574a962627de47cf18a01e2fdbae767eb692
daido1976 <daido1976@gmail.com>
2021-10-16 21:42:24 +0900

    Impl cmd::fetch_origin

--- src/cmd.rs
+++ src/cmd.rs
@@ -1,6 +1,28 @@
 use std::process::Command;
 
 pub fn show() {
+    let default_branch = get_default_branch();
+    println!("{}", default_branch);
+}
+
+pub fn rename(to_branch: &str) {
+    let from_branch = get_default_branch();
+    println!(
+        "=== START: Rename from {} to {} ===",
+        from_branch, to_branch
+    );
+    fetch_origin(&from_branch);
+    println!(
+        "=== FINISH: Rename from {} to {} ===",
+        from_branch, to_branch
+    );
+}
+
+pub fn help() {
+    println!("TODO: show help message");
+}
+
+fn get_default_branch() -> String {
     let output = Command::new("git")
         .args(["remote", "show", "origin"])
         .output()
@@ -14,14 +36,17 @@ pub fn show() {
         .split_whitespace()
         .last()
         .expect("Failed to fetch last element");
+    output.to_string()
+}
 
+fn fetch_origin(branch: &str) {
+    println!("$ git fetch origin {}", branch);
+    let output = Command::new("git")
+        .args(["fetch", "origin", branch])
+        .output()
+        .expect("Failed to execute command");
+    // git outputs the fetch and push logs to stderr...
+    // See. https://stackoverflow.com/questions/57016157/stop-git-from-writing-non-errors-to-stderr
+    let output = String::from_utf8_lossy(&output.stderr).trim().to_string();
     println!("{}", output);
-}
-
-pub fn rename(_name: &str) {
-    println!("unimplemented rename command!");
-}
-
-pub fn help() {
-    println!("TODO: show help message");
 }
