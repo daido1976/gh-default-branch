64154ef517bf292d466a1df73a9513188db434fa
daido1976 <daido1976@gmail.com>
2021-10-16 19:47:22 +0900

    Rerefactor main::run

--- src/main.rs
+++ src/main.rs
@@ -8,13 +8,14 @@ fn main() {
 
 fn run(args: Vec<String>) {
     match args.get(1) {
-        Some(first_arg) if first_arg == "show" => cmd::show(),
-        Some(first_arg) if first_arg == "rename" => match args.get(2) {
-            Some(to_name) => cmd::rename(to_name),
-            None => {
+        Some(first) if first == "show" => cmd::show(),
+        Some(first) if first == "rename" => {
+            if let Some(to_name) = args.get(2) {
+                cmd::rename(to_name)
+            } else {
                 println!("USAGE: 'gh default-branch rename <name>' Please specify the name.")
             }
-        },
+        }
         // when passed no args, or the wrong args.
         _ => cmd::help(),
     }
