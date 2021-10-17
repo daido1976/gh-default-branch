1b70546056f747ba54f27c84377c7902bfe676fa
daido1976 <daido1976@gmail.com>
2021-10-17 09:55:31 +0900

    Impl cmd::push_new_branch & cmd::rename_default_branch

--- src/cmd.rs
+++ src/cmd.rs
@@ -12,6 +12,8 @@ pub fn rename(to_branch: &str) {
         from_branch, to_branch
     );
     fetch_origin(&from_branch);
+    push_new_branch(&from_branch, to_branch);
+    rename_default_branch(to_branch);
     println!(
         "=== FINISH: Rename from {} to {} ===",
         from_branch, to_branch
@@ -49,4 +51,59 @@ fn fetch_origin(branch: &str) {
     // See. https://stackoverflow.com/questions/57016157/stop-git-from-writing-non-errors-to-stderr
     let output = String::from_utf8_lossy(&output.stderr).trim().to_string();
     println!("{}", output);
+}
+
+/// * `from` - e.g. master
+/// * `to` - e.g. main
+fn push_new_branch(from: &str, to: &str) {
+    // masterブランチからmainブランチを強制的に分岐させる
+    println!("$ git checkout -B {} origin/{} --no-track", to, from);
+    let output = Command::new("git")
+        .args([
+            "checkout",
+            "-B",
+            to,
+            &format!("origin/{}", from),
+            "--no-track",
+        ])
+        .output()
+        .expect("Failed to execute command");
+    let output = String::from_utf8_lossy(&output.stderr).trim().to_string();
+    println!("{}", output);
+
+    // mainブランチをリモートにプッシュ
+    println!("$ git push -u origin {}", to);
+    let output = Command::new("git")
+        .args(["push", "-u", "origin", to])
+        .output()
+        .expect("Failed to execute command");
+    let output = String::from_utf8_lossy(&output.stderr).trim().to_string();
+    println!("{}", output);
+
+    // リポジトリのHEADをmainブランチに切り替え
+    println!("$ git remote set-head origin {}", to);
+    let output = Command::new("git")
+        .args(["remote", "set-head", "origin", to])
+        .output()
+        .expect("Failed to execute command");
+    let output = String::from_utf8_lossy(&output.stderr).trim().to_string();
+    println!("{}", output);
+}
+
+fn rename_default_branch(to: &str) {
+    // TODO: fetch dynamically
+    let repo = "daido1976/gh-default-branch";
+    let repo = &format!("repos/{}", repo);
+    let default_branch = &format!("default_branch={}", to);
+
+    // GitHub上のデフォルトブランチをmainブランチに切り替え
+    // gh api -X PATCH "repos/${REPO}" -f default_branch=main
+    println!("$ gh api -X PATCH {} -f {}", repo, default_branch);
+    Command::new("gh")
+        .args(["api", "-X", "PATCH", repo, "-f", default_branch])
+        .output()
+        .expect("Failed to execute command");
+    println!("default branch is updated!");
+
+    // GitHub上の全てのPRのbase branchもmainブランチに切り替え
 }
