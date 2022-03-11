01fe02f48bec3aa4b42ab79e4afa661f90f2a0e5
daido1976 <daido1976@gmail.com>
2022-03-11 12:10:01 +0900

    Use precompiled binary instead of shell script

---
+++ scripts/build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+cargo build --release --locked
+mkdir -p ./dist
+
+case "$OSTYPE" in
+    darwin*)
+        EXECUTABLE=darwin-amd64
+    ;;
+    *)
+        EXECUTABLE=linux-amd64
+    ;;
+esac
+
+mv target/release/gh-report "./dist/$EXECUTABLE"
